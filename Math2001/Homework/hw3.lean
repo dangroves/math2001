366eefc4f869eea08aa85f006005f777f337f3fc
Daniel Groves <daniel.p.groves@gmail.com>
2024-09-19 14:35:25 -0500

    Fixing HW3

--- Math2001/Homework/hw3.lean
+++ Math2001/Homework/hw3.lean
@@ -13,13 +13,13 @@ Don't forget to compare with the text version,
 available as 215HW3.pdf on blackboard.
  -/
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem1 (n : ℕ) : n ≤ n^2 := by
 sorry
 
 
 
-@[autograded 4]
+@[autogradedProof 5]
 theorem problem2 (n : ℕ) : 5 ^ n ≡ 1 [ZMOD 8] ∨ 5 ^ n ≡ 5 [ZMOD 8] := by
   sorry
 
@@ -27,7 +27,7 @@ def y : ℕ → ℕ
   | 0 => 2
   | n + 1 => (y n) ^ 2
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem3 (n : ℕ) : y n = 2 ^ (2 ^ n) := by
   sorry
 
@@ -35,7 +35,7 @@ def B : ℕ → ℚ
   | 0 => 0
   | n + 1 => B n + (n + 1 : ℚ) ^ 2
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem4 (n : ℕ) : B n = n * (n + 1) * (2 * n + 1) / 6 := by
   sorry
 
@@ -44,6 +44,6 @@ def b : ℕ → ℤ
   | 1 => 1
   | n + 2 => 5 * b (n + 1) - 6 * b n
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem5 (n : ℕ) : b n = 3 ^ n - 2 ^ n := by
   sorry

81650e74f8a056c29980eb8fb222f2d44ff8eefe
Daniel Groves <daniel.p.groves@gmail.com>
2024-10-07 08:31:26 -0500

    Adding truth tables worksheet

---
+++ Math2001/Worksheets/Truth-tables-worksheet.lean
@@ -0,0 +1,38 @@
+import Library.Basic
+
+math2001_init
+set_option pp.funBinderTypes true
+
+#truth_table p ∧ q
+
+#truth_table p ∨ q
+
+#truth_table ¬ p
+
+#truth_table p →  q
+
+
+
+#truth_table ¬ p ∨ q
+
+#truth_table ¬ q → ¬ P
+
+#truth_table ¬ (p ∧ q)
+
+#truth_table ¬ p ∨ ¬ q
+
+
+
+
+#truth_table p ∨ (q ∧ r)
+
+#truth_table (p ∨ q) → p
+
+#truth_table p → (p ∨ q)
+
+#truth_table (p ∧ q) → p
+
+
+
+#truth_table P ∧ (Q ∨ R)
+#truth_table (P ∧ Q) ∨ (P ∧ R)
